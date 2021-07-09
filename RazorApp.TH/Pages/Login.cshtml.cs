2b7cb716b56b532480904e48eb48186e169c191c
Fernando del Cantao <feedc91@gmail.com>
2021-07-09 22:42:37 +0100

    differences

--- RazorApp.TH/Pages/Login.cshtml.cs
+++ RazorApp.TH/Pages/Login.cshtml.cs
@@ -23,12 +23,6 @@ namespace RazorAppTH.Pages
                 !string.IsNullOrEmpty(usuario) &&
                 !string.IsNullOrEmpty(senha))
             {
-                // Preenche as sessões - Isso será suficiente para sabermos que o usuário está logado, pois:
-                // 1. O login será autenticado através do SipWeb
-                // 2. A cada requisição à API de consumo de dados uma nova autenticação será feita novamente.
-                
-                // Para saber mais sobre o processo que invoca esta página
-                // Procure no arquivo do SipWeb no caminho: SipWeb/inc/ajax-ws.php > na função > AjaxLogin()
                 HttpContext.Session.SetString("cliente", cliente);
                 HttpContext.Session.SetString("usuario", usuario);
                 HttpContext.Session.SetString("senha", senha);
